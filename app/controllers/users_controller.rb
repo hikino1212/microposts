5df66aa768f07ae8871dd1ad5268aff90bdb4a63
hajime <ec2-user@ip-172-31-39-97.us-east-2.compute.internal>
2018-03-12 05:34:46 +0000

    follow unfollow

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,10 +23,23 @@ class UsersController < ApplicationController
     else
       flash.now[:danger] = 'ユーザの登録に失敗しました。'
       render :new
+    end
   end
-end
 
-private
+  def followings
+    @user = User.find(params[:id])
+    @followings = @user.followings.page(params[:page])
+    counts(@user)
+  end
+  
+  def followers
+    @user = User.find(params[:id])
+    @followers = @user.followers.page(params[:page])
+    counts(@user)
+  end
+
+  private
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
+end
